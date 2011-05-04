be53ad33fe29070f0b8ddcfb4f50b9bd7c84e363
lazcorp <lazcorp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-04 15:24:24 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/wms-tools/trunk@380796 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== WMS Tools ===
+Tags: webmaster,seo,wms,tools,webmaster tools,
+Requires at least: 2.5
+Tested up to: 3.3.3
+Stable tag: trunk
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=2545483
+Contributors: mlazarov, lazcorp
+
+WMS Tools is the easeast way to install wms-tools.com auth code
+
+== Description ==
+
+WMS Tools plugin helps you to add wms-tools.com auth code
+
+
+
+
+== Installation ==
+
+1. Put the plugin folder into [wordpress_dir]/wp-content/plugins/
+2. Go into the WordPress admin interface and activate the plugin
+3. Optional: go to the options page and configure the plugin
+
+Before upgrade DEACTIVATE the plugin and then ACTIVATE and RECONFIGURE!
+
+== Frequently Asked Questions ==
+
+**How can I submit a bug?**
+
+Write me to lazarov@mail.bg, please, it's the quicker way to have it fixed. You can write to the 
+WordPress forum, too, but I read it rarely.
+
+**Other FAQs?**
+
+I'm collection tips and FAQ on [this page](http://wordpress.org/extend/plugins/wms-tools/) 
+so I can update it more easly.
+    
+== Screenshots ==
+
+No screenshots are available.
---
+++ wms-tools.php
@@ -0,0 +1,124 @@
+<?php
+/*
+Plugin Name: WMS Tools
+Plugin Script: wms-tools.php
+Plugin URI: http://marto.lazarov.org/plugins/wms-tools
+Description: Connect your wordpress blog to wms-tools.com
+Version: 1.0.3
+Author: mlazarov
+Author URI: http://marto.lazarov.org
+Min WP Version: 2.7
+Max WP Version: 3.2
+Update Server: http://marto.lazarov.org/plugins/wms-tools
+
+== Changelog ==
+= 1.0.3 =
+* Bugfixes
+
+= 1.0.0 =
+* First release
+
+*/
+
+if (!class_exists('wms_tools')) {
+	class wms_tools {
+	
+		function wms_tools() {
+			$this->__construct();
+			
+		}
+		function __construct() {
+			$stored_options = get_option('wms_tools_options');
+			
+			$this->options = (array)(is_serialized($stored_options)) ? unserialize($stored_options) : $stored_options;
+			
+			// Setting filters, actions, hooks....      
+			add_action("admin_menu", array (
+				& $this,
+				"admin_menu_link"
+			));
+			
+			add_action('wp_footer', array(&$this,'footer'));
+
+		}
+
+		// -----------------------------------------------------------------------------------------------------------	
+		/**
+		* @desc Adds the options subpanel
+		*/
+		function admin_menu_link() {
+			add_management_page('WMS Tools', 'WMS Tools', 8, basename(__FILE__), array (
+				& $this,
+				'admin_options_page'
+			));
+			add_filter('plugin_action_links_' . plugin_basename(__FILE__), array (
+				& $this,
+				'filter_plugin_actions'
+			), 10, 2);
+		}
+
+		// -----------------------------------------------------------------------------------------------------------	
+		/**
+		* Adds the Settings link to the plugin activate/deactivate page
+		*/
+		function filter_plugin_actions($links, $file) {
+			$settings_link = '<a href="tools.php?page=' . basename(__FILE__) . '">' . __('Settings') . '</a>';
+			array_unshift($links, $settings_link); // before other links
+
+			return $links;
+		}
+		
+		function Footer(){
+			if($this->options['user_code']){
+			?>
+
+<script src="http://wms-tools.com/kanalytics.js" type="text/javascript"></script>
+<script type="text/javascript">
+<!--
+kanalytics(<?=$this->options['user_code'];?>);
+//-->
+</script>
+			
+<?php
+			}
+		}
+
+		// -----------------------------------------------------------------------------------------------------------	
+		/**
+		* Administration options page
+		*/
+		function admin_options_page() {
+			global $wpdb;
+			
+			if ($_POST['wms_tools']) {
+				$this->options['user_code'] = (int)$_POST['user_code'];
+				update_option('wms_tools_options', serialize($this->options));
+			
+			}
+			
+			?>
+			<div class="wrap">
+				<div id="dashboard" style="width:150px;padding:10px;">
+					<h3>WMS Tools</h3>
+					<form method="post">
+						<div  style=""> 
+							User Code:<br/>
+							<input type="text" name="user_code" value="<?=$this->options['user_code'];?>" size="8"/>
+							<input type="submit" name="wms_tools" class="button-primary" value="Save" />
+						</div>
+					</form>
+				</div>
+				
+				
+				
+			</div>
+			<?php
+		}
+
+	} //End Class
+}
+
+if (class_exists('wms_tools')) {
+	$wp_delete_posts_var = new wms_tools();
+}
+?>
